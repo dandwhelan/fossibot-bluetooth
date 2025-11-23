27b28f878f97744f60bceec6147325f9cbde9f57
dandwhelan <dandwhelan@gmail.com>
2025-11-23 18:17:02 +0000

    Initial commit of Fossibot Control PWA

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'fossibot-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    'https://cdn.tailwindcss.com',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request))
+    );
+});
